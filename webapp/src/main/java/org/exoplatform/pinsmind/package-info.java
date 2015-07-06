d0b0508d3d85d29822405438075837f66f51fa94
maydt <maydt@exoplatform.com>
2015-07-06 19:08:26 +0700

    Add resources

--- webapp/src/main/java/org/exoplatform/pinsmind/package-info.java
+++ webapp/src/main/java/org/exoplatform/pinsmind/package-info.java
@@ -2,24 +2,27 @@
 @Portlet(name="PinsMindPortlet")
 @Bindings({})
 
-/*
-@Scripts(location = AssetLocation.SERVER,
-        value = {
-            @Script(value = "js/jquery-1.8.3.min.js", id = "jquery"),
-            @Script(value = "js/jquery-juzu-utils-0.1.0.js", depends = "jquery", id = "juzu-utils"),
-            @Script(value = "js/notif.js", id = "notif", depends = {"jquery", "juzu-utils"})
-        }
-)
+@Scripts({
+    @Script(value = "jquery.min.js", id = "jquery"),
+    @Script(value = "jquery-ui.min.js", id = "jqueryui"),
+    @Script(value = "raphael-min.js", id = "raphaelmin", depends = {"jquery", "jqueryui"}),
+    @Script(value = "js-mindmap.js", id = "jsmindmap", depends = {"jquery", "jqueryui","raphaelmin"}),
+    @Script(value = "script.js", id = "scriptnormal", depends = {"jquery", "jqueryui","raphaelmin","jsmindmap"})
+  })
+
 @Stylesheets({
-        @Stylesheet(value = "css/style.css", location = AssetLocation.SERVER)
+  @Stylesheet(value = "style.css"),
+  @Stylesheet(value = "js-mindmap.css")
 })
-*/
+
+@Assets("*")
 package org.exoplatform.pinsmind;
 
 import juzu.Application;
-//import juzu.asset.AssetLocation;
-//import juzu.plugin.asset.Assets;
-//import juzu.plugin.asset.Script;
-//import juzu.plugin.binding.Binding;
+import juzu.plugin.asset.Assets;
+import juzu.plugin.asset.Scripts;
+import juzu.plugin.asset.Script;
+import juzu.plugin.asset.Stylesheet;
+import juzu.plugin.asset.Stylesheets;
 import juzu.plugin.binding.Bindings;
 import juzu.plugin.portlet.Portlet;
